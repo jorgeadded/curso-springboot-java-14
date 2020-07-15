1472221e3e7c37178ac4086f2c64e93bf9121d1f
jorgeadded <jorgeadded@gmail.com>
2020-07-15 20:44:13 -0300

    projeto criado

---
+++ src/main/java/com/jorge/curso_sts/CursoStsApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.curso_sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoStsApplication.class, args);
+	}
+
+}
